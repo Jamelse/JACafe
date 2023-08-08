0933366f3d09c0fb32ab5f44e3813399e5397a8e
Jacob <jacobamelse@hotmail.com>
2023-08-08 12:39:18 -0500

    added controller methods to user controller (create, show, destroy)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+
+  def create 
+    user = User.create!(user_params)
+      session[:user_id] = user.id
+      render json: user, status: :created
+  end
+
+  def show
+    user = User.find_by(id: session[:user_id])
+    render json: user, status: :created
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update!(user_params)
+    render json: user, status: :accepted
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :first_name, :last_name, :income)
+  end
+
 end
