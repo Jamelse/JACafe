17d2c5d9132eac7fc6cfc33669e19eee2271a90a
Jacob <jacobamelse@hotmail.com>
2023-08-12 20:05:57 -0500

    added more routing logic for signup, login, logout. removed unused routes and routes from resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  resources :cart_items
-  resources :carts
+  resources :carts, only: :update
   patch '/carts/:id/new_quantity', to: 'carts#new_quantity'
   patch '/carts/:id/delete_item', to: 'carts#delete_item'
-  resources :orders
-  resources :coffee_details
+  resources :orders, only: [:index, :update]
+  get '/user_orders', to: 'orders#user_orders'
   resources :coffees
-  resources :users
+  resources :users, only: [:show, :create, :update]
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
