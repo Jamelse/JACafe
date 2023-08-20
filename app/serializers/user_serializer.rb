6dd7de92a0bf5478f8b5a3f793b5d14d9120663e
Jacob <jacobamelse@hotmail.com>
2023-08-20 01:06:06 -0500

    added orders to users serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :isAdmin, :email, :first_name, :last_name, :stripe_id
+  has_many :orders
 end
--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -8,7 +8,7 @@ function Cart({cart}){
     <div>
       {cart ? cart.cart_items.map(item => {
         return (
-          <div>
+          <div key={item.id}>
             <h2>{item.item_summary.name}</h2>
             <h1>{item.quantity}</h1>
             <h2>{item.total_cost}</h2>
